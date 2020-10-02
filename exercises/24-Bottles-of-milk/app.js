0118447502963caa4be03de858f92534f1d2a234
amafjarkasi <amafjarkasi@gmail.com>
2020-10-02 23:13:58 +0000

    fixes

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -4,20 +4,17 @@ let bottleLyrics;
 function bottlesMilk() {
     bottleLyrics = "";
 
-    for (var bottleCount = 100; bottleCount > 0; bottleCount--) {
+    for (let bottleCount = 99; bottleCount >= 1; bottleCount--) {
 
         if (bottleCount === 1) {
-            bottleLyrics = bottleLyrics + `${bottleCount} bottles of milk on the wall, ${bottleCount} of milk. Take one down and pass it around, no more bottles of milk on the wall. `;
+            console.log(`${bottleCount} bottle of milk, ${bottleCount} of milk. Take one down and pass it around, no more bottles of milk on the wall.\n\nNo more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more.`);
             break;
         }
 
-        bottleLyrics = bottleLyrics + `${bottleCount} bottles of milk on the wall, ${bottleCount} of milk. Take one down and pass it around. `; 
+        console.log(`${bottleCount} bottles of milk on the wall, ${bottleCount} of milk. Take one down and pass it around.\n\n`);
     }
 
-        bottleLyrics = bottleLyrics + 'No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more.'; 
-
-
-    return bottleLyrics;
+    return;
 }
 
-console.log(bottlesMilk());
+bottlesMilk();
